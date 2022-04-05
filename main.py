4d352e56c61d65a0b0dace57174bb55dc3410fc7
ClayMix <claymix007@gmail.com>
2022-04-05 23:50:52 +0500

    Creating simple Flask server

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-# There's going to be server for my Group School Project
+from flask import Flask, request
+from flask_restful import abort, Api
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey?'
+api = Api(app)
+
+
+# May be I'll add some api URLs
+# api.add_resource()
+
+@app.route('/')
+def main_page():
+    return "Hi"
+
+
+if __name__ == '__main__':
+    app.run()
