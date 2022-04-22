4da17cbcca41b1d34f25892e8e6ab346ec080021
ClayMix <claymix007@gmail.com>
2022-04-22 22:36:56 +0500

    Add some base funcs for http requests

--- data/server_api.py
+++ data/server_api.py
@@ -8,3 +8,39 @@ blueprint = flask.Blueprint(
     __name__,
     template_folder='templates'
 )
+
+
+@blueprint.route('/api/users')
+def get_users():
+    return jsonify(
+        {
+            'error': 'error'
+        }
+    )
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    return jsonify(
+        {
+            'error': 'error'
+        }
+    )
+
+
+@blueprint.route('/api/users', methods=['POST'])
+def create_user():
+    return jsonify(
+        {
+            'error': 'error'
+        }
+    )
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    return jsonify(
+        {
+            'error': 'error'
+        }
+    )
