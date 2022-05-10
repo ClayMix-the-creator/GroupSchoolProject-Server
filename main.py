56dd8224a446503aead5577365f0aaf04fbbb762
ClayMix <claymix007@gmail.com>
2022-05-10 17:47:29 +0500

    Another try

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from data import db_session, server_api
 def main():
     db_session.global_init('db/all_users.sqlite')
     app.register_blueprint(server_api.blueprint)
-    app.run()
+    app.run(port=5000)
 
 
 if __name__ == '__main__':
