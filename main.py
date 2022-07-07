66c4999a867940e3953e50f3a42046908483a2d0
ClayMix <claymix007@gmail.com>
2022-07-07 21:59:28 +0500

    port

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from server import app
 from data import db_session, server_api
 import os
 
-ON_HEROKU = os.environ.get('ON_HEROKU')
-
 
 def main():
     db_session.global_init('db/all_users.sqlite')
     app.register_blueprint(server_api.blueprint)
-    app.run(port=int(os.environ.get('PORT', 17995)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
